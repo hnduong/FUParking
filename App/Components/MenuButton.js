dca47f4097ab02ba24939be7e59c3ee9aaae861e
Hai Duong <hnduong94@gmail.com>
2018-01-29 19:12:57 -0800

    animated menu button some more

--- App/Components/MenuButton.js
+++ App/Components/MenuButton.js
@@ -1,12 +1,33 @@
 import React from 'react'
 import Lottie from 'lottie-react-native'
-import { StyleSheet, TouchableOpacity } from 'react-native'
+import * as Animatable from 'react-native-animatable'
+import { Animated, StyleSheet, TouchableOpacity } from 'react-native'
 
-const menuJSON = require('../Resources/Lottie/menu.json')
+const menuJSON = require('../Resources/Lottie/menu3.json')
 
 class MenuButton extends React.Component {
+  state = {
+    progress: new Animated.Value(0)
+  }
+
+  componentDidMount () {
+    Animated.timing(this.state.progress, {
+      toValue: 0.5,
+      duration: 2000
+    }).start()
+    this.animatedView.slideInLeft(1000)
+  }
+
   handleOnPress = () => {
-    this.animation.play()
+    Animated.sequence([
+      Animated.timing(this.state.progress, {
+        toValue: 1,
+        duration: 200
+      })
+    ]).start()
+    setTimeout(() => {
+      this.setState({ progress: new Animated.Value(0.5) })
+    }, 200)
   }
   render () {
     return (
@@ -14,14 +35,19 @@ class MenuButton extends React.Component {
         style={[styles.button]}
         onPress={this.handleOnPress}
       >
-        <Lottie
+        <Animatable.View
+          ref={(ref) => { this.animatedView = ref }}
           style={[StyleSheet.absoluteFill]}
-          ref={animation => { this.animation = animation }}
-          source={menuJSON}
-          loop={false}
-          speed={2}
-          enableMergePathsAndroidForKitKatAndAbove
-        />
+        >
+          <Lottie
+            style={[StyleSheet.absoluteFill]}
+            progress={this.state.progress}
+            source={menuJSON}
+            loop={false}
+            speed={1}
+            enableMergePathsAndroidForKitKatAndAbove
+          />
+        </Animatable.View>
       </TouchableOpacity>
     )
   }
